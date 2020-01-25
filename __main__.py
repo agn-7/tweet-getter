451d193c0d9791b6aaa634dfad7fe01962fe7b6e
benyamin jafari <benyaminjmf@gmail.com>
2020-01-25 09:28:07 +0330

    __main__ added

---
+++ __main__.py
@@ -0,0 +1,13 @@
+"""
+Emotion Detection Based on Context.
+"""
+
+__author__ = 'Benyamin Jafari & Mahsa Mozhdehi'
+__copyright__ = "Copyright 2020, Planet Earth"
+__version__ = "1.0.1"
+__maintainer__ = "aGn"
+__email__ = "benyaminjmf@gmail.com"
+
+
+if __name__ == '__main__':  # TODO
+    ...
