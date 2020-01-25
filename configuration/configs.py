cd439180635234d9694d3683bd58c1c8da545c96
benyamin jafari <benyaminjmf@gmail.com>
2020-01-25 09:28:41 +0330

    parametric configuration added

---
+++ configuration/configs.py
@@ -0,0 +1,14 @@
+"""
+Configurations
+"""
+
+DATA_PREPARATION = {
+    'api_key': None,
+    'api_secret': None,
+    'access_token': None,
+    'access_token_secret': None,
+    'chunked_start': None,
+    'chunked_end': None,
+}
+
+MODEL_PARAMS = {}
